9fb11db7962e1d69c43f0f377219dbc3486a941f
Scott Guelich <scott@guelich.net>
2014-02-03 11:45:32 -0500

    Bump copyright dates

--- SampleApplication/Source/AppDelegate.h
+++ SampleApplication/Source/AppDelegate.h
@@ -2,7 +2,7 @@
 //  AppDelegate.h
 //  SGTouchPresenter
 //
-//  Copyright 2011-2012 Scott Guelich.
+//  Copyright 2011-2014 Scott Guelich.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a
 //  copy of this software and associated documentation files (the "Software"),
